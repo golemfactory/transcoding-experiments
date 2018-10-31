74333155e3d0b3c5426a2438d2dfa955701adf69
nieznany.sprawiciel <nieznany.sprawiciel@gmail.com>
2018-10-31 16:55:37 +0100

    ffmpeg merge streams (works bad))

--- ffmpeg_commands.py
+++ ffmpeg_commands.py
@@ -9,6 +9,10 @@ FFMPEG_COMMAND = "ffmpeg"
 ######################################
 ##
 def exec_cmd(cmd):
+
+    print( "Executing command:" )
+    print( cmd )
+
     pc = subprocess.Popen(cmd)
     return pc.wait()
 
@@ -35,8 +39,54 @@ def extract_video_part_command( input, output, start_time, end_time ):
 
 ######################################
 ##
+def generate_merge_list( input_files, output ):
+
+    working_dir = os.getcwd()
+
+    content = ""
+    for file in input_files:
+        file_abs_path = os.path.join( working_dir, file )
+        content += "file '" + file_abs_path + "'\n"
+
+    output_list_file = output + ".mergelist"
+
+    with open(output_list_file, 'w') as file:
+        file.write(content)
+
+    return output_list_file
+    
+
+######################################
+##
+def merge_videos_command( input_files, output ):
+
+    list_file = generate_merge_list( input_files, output )
+
+    cmd = [ FFMPEG_COMMAND,
+        "-f", "concat",
+        "-safe", "0",
+        "-i", list_file,
+        "-c", "copy", output
+    ]
+
+    return cmd, list_file
+
+
+######################################
+##
 def extract_video_part( input, output, start_time, end_time ):
 
     cmd = extract_video_part_command( input, output, start_time, end_time )
     exec_cmd( cmd )
+
+######################################
+##
+def merge_videos( input_files, output ):
+
+    cmd, list_file = merge_videos_command( input_files, output )
+    exec_cmd( cmd )
+
+    # remove temporary file with merge list
+    #os.remove( list_file )
+
 
---
+++ merge_av.py
@@ -0,0 +1,31 @@
+import sys
+import os
+
+import ffmpeg_commands as ffmpeg
+from split_av import split_video
+
+
+
+
+
+######################################
+##
+def run():
+
+    file_name = sys.argv[ 1 ]
+    output_file = sys.argv[ 2 ]
+    num_splits = int( sys.argv[ 3 ] )
+    video_len = int( sys.argv[ 4 ] )       # We should read this from file
+
+    output_dir = os.path.dirname( output_file )
+
+    results = split_video( file_name, output_dir, num_splits, video_len )
+    print( results )
+
+    ffmpeg.merge_videos( results, output_file )
+
+
+if __name__ == "__main__":
+    run()
+
+
