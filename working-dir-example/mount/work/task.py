3e1a2c3904da4931a425bf06e0af66dd34efa9e5
Daniel Bigos <daniel.bigos@golem.network>
2018-12-05 13:26:25 +0100

    refactor: change params to json format

--- working-dir-example/mount/work/task.py
+++ working-dir-example/mount/work/task.py
@@ -1,10 +1,11 @@
-import params
+import json
 import m3u8
 import os
 from ffmpeg_commands import merge_videos, split_video, transcode_video
 from m3u8_utils import create_and_dump_m3u8, join_playlists
 
 OUTPUT_DIR = "/golem/output"
+PARAMS_FILE = "params.json"
 
 def split_stream(path_to_stream, video_length, parts):
     split_file = split_video(path_to_stream, OUTPUT_DIR, video_length / parts)
@@ -26,13 +27,14 @@ def merge_stream(playlists_dir, outputfilename):
     merge_videos(merged_filename, outputfilename)
 
 def run():
-
-    if params.context == 1:
-        split_stream(params.path_to_stream, params.video_length, params.parts)
-    elif params.context == 2:
-        transcode_segment(params.playlist, params.targs)
-    elif params.context == 3:
-        merge_stream(params.playlists_dir, params.output_stream)
-
+    with open("params.json", 'r') as f:
+        params = json.load(f)
+        if params['context'] == "1":
+            split_stream(params['path_to_stream'], params['video_length'], params['parts'])
+        elif params['context'] == "2":
+            transcode_segment(params['playlist'], params['targs'])
+        elif params['context'] == "3":
+            merge_stream(params['playlists_dir'], params['output_stream'])
+    
 if __name__ == "__main__":
     run()
