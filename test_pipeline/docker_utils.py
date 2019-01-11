0f1e633f7fea145a2fce656c630cd1f4695b9a05
nieznany.sprawiciel <nieznany.sprawiciel@gmail.com>
2019-01-11 16:17:00 +0100

    Split pipeline step rewritten to python

---
+++ test_pipeline/docker_utils.py
@@ -0,0 +1,73 @@
+import os
+import subprocess
+import shutil
+
+
+def exec_cmd(cmd, file=None):
+    print("Executing command:")
+    print(cmd)
+
+    pc = subprocess.Popen(cmd, stdout=file)
+    return pc.wait()
+
+
+def default_golem_mounts(host_dir):
+    
+    mount_list = []
+
+    mount_list += [ [ os.path.join(host_dir, "work"), "/golem/work/" ] ]
+    mount_list += [ [ os.path.join(host_dir, "output"), "/golem/output/" ] ]
+    mount_list += [ [ os.path.join(host_dir, "resources"), "/golem/resources/" ] ]
+
+    return mount_list
+
+
+def create_environment(host_dir, mount_dirs, work_files, resource_files):
+
+    # Create directories that will be mounted to docker filesystem
+    for mount_dir in mount_dirs:
+        if not os.path.exists(mount_dir[0]):
+            os.makedirs(mount_dir[0])
+
+
+    work_dir = os.path.join( host_dir, "work" )
+    for file in work_files:
+        destination = os.path.join( work_dir, os.path.basename(file))
+        shutil.copyfile(file, destination)
+
+    resources_dir = os.path.join( host_dir, "resources" )
+    for file in resource_files:
+        destination = os.path.join( resources_dir, os.path.basename(file))
+        shutil.copyfile(file, destination)
+
+
+def docker_mount_command(mount_dirs):
+
+    cmd = []
+    for mount_dir in mount_dirs:
+        
+        # Can't mount not existing host path. Create to be sure it exists.
+        if not os.path.exists(mount_dir[0]):
+            os.makedirs(mount_dir[0])
+
+        single_mount = [ "--mount" ]
+        single_mount += [ "type=bind,source=" + mount_dir[0] + ",target=" + mount_dir[1] ]
+
+        cmd += single_mount
+
+    return cmd
+
+
+def run(image, script, mount_dirs):
+
+    cmd = [ "docker", "run" ]
+    cmd += [ "-it", "--rm" ]
+    cmd += docker_mount_command(mount_dirs)
+    cmd += [ image ]
+    cmd += [ script ]
+
+    exec_cmd(cmd)
+
+
+
+
---
+++ test_pipeline/pipeline.py
@@ -0,0 +1,77 @@
+import os
+import glob
+import json
+import docker_utils as docker
+
+
+
+PARAMS_TMP="working-dir/tmp/work/params.json"
+SCRIPT="working-dir-example/mount/work/task.py"
+
+
+
+def load_params(file):
+    with open(file, 'r') as f:
+        params = json.load(f)
+    return params
+
+
+def save_params(params, output):
+
+    dirname = os.path.dirname(output)
+    if not os.path.exists(dirname):
+        os.makedirs(dirname)
+
+    with open(output, 'w+') as f:
+        json.dump(params, f)
+
+
+def split_video(task_def, tests_dir, image):
+
+    print("Splitting...")
+
+    # Create split command
+    params = dict( task_def )
+    params[ "command" ] = "split"
+    del params[ "host_stream_path" ]        # Task definition for docker doesn't have this field.
+
+    save_params( params, PARAMS_TMP )
+
+    # Prepare files that should be copied to docker environment (mounted directories).
+    work_files = [
+        SCRIPT,
+        PARAMS_TMP
+    ]
+
+    resource_files = [
+        task_def[ "host_stream_path" ],
+    ]
+
+    # These commands will prepare environment for docker to run.
+    mounts = docker.default_golem_mounts( tests_dir )
+    docker.create_environment( tests_dir, mounts, work_files, resource_files )
+
+    # Run docker
+    docker.run(image, "task.py", mounts)
+
+
+def run_pipeline(task_def, tests_dir, image):
+
+    split_video(task_def, os.path.join( tests_dir, "split" ), image )
+
+
+
+def run():
+
+    PARAMS="working-dir/mount/work/params.json"
+
+    task_def = load_params(PARAMS)
+    tests_dir = os.path.join( os.getcwd(), "working-dir/test/" )
+
+    run_pipeline(task_def, tests_dir, "golemfactory/ffmpeg:0.2")
+
+
+if __name__ == "__main__":
+    run()
+
+
