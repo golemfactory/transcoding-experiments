d5e1ffe856a9a7171ac818df6b03d615e2898837
nieznany.sprawiciel <nieznany.sprawiciel@gmail.com>
2019-01-23 13:33:27 +0100

    Replace old scripts

--- keyframes.sh
+++ keyframes.sh
@@ -1,18 +1,3 @@
 #!/usr/bin/env bash
 
-IMAGE=$1
-
-function print_keyframes() {
-     docker run -it --rm --entrypoint ffprobe \
-    --mount type=bind,source="$(pwd)"/working-dir/mount/work/,target=/golem/work/ \
-    --mount type=bind,source="$(pwd)"/working-dir/mount/output/,target=/golem/output/ \
-    --mount type=bind,source="$(pwd)"/working-dir/mount/resources/,target=/golem/resources/ \
-    ${IMAGE} \
-    -loglevel quiet \
-    -skip_frame nokey \
-    -select_streams v:0 \
-    -show_entries frame=pkt_pts_time \
-    -of csv=print_section=0 $1
-}
-
-print_keyframes $2
+ffprobe -loglevel quiet -skip_frame nokey -select_streams v:0 -show_entries frame=pkt_pts_time -of csv=print_section=0 $1
--- print-keyframes.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-
-ffprobe -loglevel quiet -skip_frame nokey -select_streams v:0 -show_entries frame=pkt_pts_time -of csv=print_section=0 $1
