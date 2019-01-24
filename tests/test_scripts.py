afb18b1721a456b7f78f6e0d99ff14e1dd9d1395
nieznany.sprawiciel <nieznany.sprawiciel@gmail.com>
2019-01-24 17:20:38 +0100

    Fixed problem with quering video length

--- tests/test_scripts.py
+++ tests/test_scripts.py
@@ -11,11 +11,11 @@ import ffmpeg_commands as ffmpeg
     ("tests/videos/different-codecs/big-buck-bunny-[codec=theora].ogv", 60.708333),
     ("tests/videos/different-codecs/big-bunny-[codec=flv1].flv", 54.082),
     ("tests/videos/different-codecs/carphone_qcif-[codec=rawvideo].y4m", 12.746067),
-    ("tests/videos/different-codecs/Dance-[codec=mpeg2video].mpeg", 30.166667),
-    ("tests/videos/different-codecs/ForBiggerBlazes-[codec=h264].mp4", 14.995000),
-    ("tests/videos/different-codecs/ForBiggerMeltdowns-[codec=mpeg4].mp4", 15.020833),
+    ("tests/videos/different-codecs/Dance-[codec=mpeg2video].mpeg", 30.196733),
+    ("tests/videos/different-codecs/ForBiggerBlazes-[codec=h264].mp4", 15.021667),
+    ("tests/videos/different-codecs/ForBiggerMeltdowns-[codec=mpeg4].mp4", 15.045),
     ("tests/videos/different-codecs/Panasonic-[codec=vp9].webm", 46.12),
-    ("tests/videos/different-codecs/star_trails-[codec=wmv2].wmv", 21.2490),
+    ("tests/videos/different-codecs/star_trails-[codec=wmv2].wmv", 21.292),
     ("tests/videos/different-codecs/TRA3106-[codec=h263].3gp", 16.9840)
 ])
 def test_get_video_len(videofile, expected_length):
