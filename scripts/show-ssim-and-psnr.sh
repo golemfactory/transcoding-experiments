d4b482892085ca83de50b0c306fde4008956c5b9
cameel <cameel2@gmail.com>
2019-03-04 18:55:48 +0100

    Scripts for diffing videos

---
+++ scripts/show-ssim-and-psnr.sh
@@ -0,0 +1,11 @@
+#!/bin/bash -e
+
+reference_file="$1"
+new_file="$2"
+
+ffmpeg                            \
+    -i "$reference_file"          \
+    -i "$new_file"                \
+    -lavfi  "ssim;[0:v][1:v]psnr" \
+    -f null                       \
+    -
