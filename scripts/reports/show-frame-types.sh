dd1768b297719b9b3f400b273adadbff4144aa63
cameel <cameel2@gmail.com>
2019-03-08 02:57:06 +0100

    show-frame-types report: use cached frame types and input files copied to the experiment dir

--- scripts/reports/show-frame-types.sh
+++ scripts/reports/show-frame-types.sh
@@ -2,24 +2,18 @@
 
 source functions/all.sh
 
-experiment_name="$1"
-video_file="$2"
+experiment_dir="$1"
 
-video_basename="$(basename "$video_file")"
-experiment_dir="output/$experiment_name/$video_basename"
-output_format="$(get_extension "$video_file")"
+input_format="$(cat "$experiment_dir/input-format")"
+output_format="$(cat "$experiment_dir/output-format")"
+input_file="$experiment_dir/input.$input_format"
 merged_file="$experiment_dir/merged.$output_format"
 
-input_frame_types="$(frame_types "$video_file")"
-merged_frame_types="$(frame_types "$merged_file")"
+input_frame_types="$(load_frame_types_for_video "$input_file")"
+merged_frame_types="$(load_frame_types_for_video "$merged_file")"
+segment_frame_types="$(frame_types_from_all_segments_side_by_side "$experiment_dir/split")"
 
-segment_frame_types=""
-for segment_basename in $(cat "$experiment_dir/split/segments.txt"); do
-    segment_file="$experiment_dir/split/$segment_basename"
-    segment_frame_types="$segment_frame_types$(frame_types "$segment_file")"
-done
-
-echo "Frame types from experiment '$experiment_name' on $video_file"
+echo "Frame types from '$experiment_dir'"
 printf "Input video:\n%s\n"               "$input_frame_types"
 printf "All segments side by side:\n%s\n" "$segment_frame_types"
 printf "Merged video:\n%s\n"              "$merged_frame_types"
