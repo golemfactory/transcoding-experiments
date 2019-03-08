892a818231f070ffd110906062b2054c2d4dadec
cameel <cameel2@gmail.com>
2019-03-08 11:55:02 +0100

    Reorganize report columns to have fewer tables

--- scripts/run-reports.sh
+++ scripts/run-reports.sh
@@ -4,13 +4,18 @@ source functions/all.sh
 
 output_dir="$1"
 
-echo "General info report"
+echo "Input file info"
 columns=(
     input_video_name
     input_num_streams
     input_video_format
     input_video_codec
     input_duration_rounded
+    input_frame_count
+    input_unique_frame_types
+    input_i_frame_count
+    input_p_frame_count
+    input_b_frame_count
 )
 print_report "$output_dir/segment-split-only" ${columns[@]}
 echo
@@ -35,36 +40,48 @@ echo "Timestamp report for segment-split-vp9-convert"
 print_report "$output_dir/segment-split-vp9-convert" ${timestamps_transcode_merge_columns[@]}
 echo
 
-echo "Frame type (input, output, merged) report for segment-split-half-scale"
-frame_types_transcode_merge_columns=(
+echo "Frame type report for segment-split-half-scale"
+frame_types_with_transcoding_columns=(
     input_video_name
     input_frame_count
     output_frame_count
+    segment_frame_count
+    transcoded_segment_frame_count
     merged_frame_count
     input_unique_frame_types
     output_unique_frame_types
+    segment_unique_frame_types
+    transcoded_segment_unique_frame_types
     merged_unique_frame_types
     input_i_frame_count
     output_i_frame_count
+    segment_i_frame_count
+    transcoded_segment_i_frame_count
     merged_i_frame_count
     input_p_frame_count
     output_p_frame_count
+    segment_p_frame_count
+    transcoded_segment_p_frame_count
     merged_p_frame_count
     input_b_frame_count
     output_b_frame_count
+    segment_b_frame_count
+    transcoded_segment_b_frame_count
     merged_b_frame_count
     input_same_frame_types_as_output
     output_same_frame_types_as_merged
+    input_same_frame_types_as_segments
+    segments_same_frame_types_as_merged
 )
-print_report "$output_dir/segment-split-half-scale" ${frame_types_transcode_merge_columns[@]}
+print_report "$output_dir/segment-split-half-scale" ${frame_types_with_transcoding_columns[@]}
 echo
 
-echo "Frame type (input, output, merged) report for segment-split-vp9-convert"
-print_report "$output_dir/segment-split-vp9-convert" ${frame_types_transcode_merge_columns[@]}
+echo "Frame type report for segment-split-vp9-convert"
+print_report "$output_dir/segment-split-vp9-convert" ${frame_types_with_transcoding_columns[@]}
 echo
 
-echo "Frame type (input, segments, merged) report for segment-split-only"
-frame_types_merge_split_columns=(
+echo "Frame type report for segment-split-only"
+frame_types_without_transcoding_columns=(
     input_video_name
     input_frame_count
     segment_frame_count
@@ -84,26 +101,8 @@ frame_types_merge_split_columns=(
     input_same_frame_types_as_segments
     segments_same_frame_types_as_merged
 )
-print_report "$output_dir/segment-split-only" ${frame_types_merge_split_columns[@]}
+print_report "$output_dir/segment-split-only" ${frame_types_without_transcoding_columns[@]}
 echo
-
-echo "Frame type report for segment-split-half-scale"
-print_report "$outputdir/segment-split-half-scale" ${frame_types_merge_split_columns[@]}
-echo
-
-echo "Frame type report"
-frame_types_columns=(
-    input_video_name
-    input_frame_count
-    input_unique_frame_types
-    input_i_frame_count
-    input_p_frame_count
-    input_b_frame_count
-    input_duration_rounded
-)
-print_report "$output_dir/segment-split-only" ${frame_types_columns[@]}
-echo
-
 
 echo "Frame type comparison between input video, segment videos and merged video (split without transcoding)"
 for video_file in $(ls -1 "$output_dir/segment-split-only"); do
