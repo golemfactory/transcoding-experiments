07ade0e481a379205a84433e3513b1f89e8834d2
cameel <cameel2@gmail.com>
2019-03-08 10:27:19 +0100

    Script for generating a set of videos consisting of numbered frames

---
+++ scripts/generate-number-videos.sh
@@ -0,0 +1,42 @@
+mkdir --parents number-frames/
+mkdir --parents number-videos/
+
+num_frames=100
+
+echo "Generating $num_frames number frames"
+
+# NOTE: The H.263 codec supports only a few specific resolutions. 352x288 is one of them.
+for i in $(seq 1 "$num_frames"); do
+    convert                 \
+        -gravity    Center  \
+        -background blue    \
+        -fill       white   \
+        -pointsize  200     \
+        label:$i            \
+        -extent     352x288 \
+        -quality    100     \
+        number-frames/$i.png
+done
+
+function generate_number_video {
+    local codec="$1"
+    local format="$2"
+
+    echo "Generating number-videos/numbers-$codec.$format"
+
+    ffmpeg                                     \
+        -nostdin                               \
+        -v error                               \
+        -i number-frames/%d.png                \
+        -vcodec "$codec"                       \
+        "number-videos/numbers-$codec.$format"
+}
+
+generate_number_video vp9    mp4
+generate_number_video theora ogv
+generate_number_video flv1   flv
+generate_number_video mpeg4  mpeg
+generate_number_video h264   mp4
+generate_number_video vp9    webm
+generate_number_video wmv2   asf
+generate_number_video h263   3gp
