f4a37ace1d47664e8711dff79b0aea0c1108b752
cameel <cameel2@gmail.com>
2019-03-08 22:07:31 +0100

    Rename ffmpeg_concat to ffmpeg_concat_demuxer

--- scripts/experiments/segment-split-vp9-convert.sh
+++ scripts/experiments/segment-split-vp9-convert.sh
@@ -13,6 +13,6 @@ init_experiment_dir       "$experiment_dir" "$video_file" mkv
 split_input               "$experiment_dir" "split_with_ffmpeg_segment $num_segments"
 transcode_input           "$experiment_dir" "ffmpeg_transcode_with_codec vp9"
 transcode_segments        "$experiment_dir" "ffmpeg_transcode_with_codec vp9"
-merge_transcoded_segments "$experiment_dir" "merge_with_ffmpeg_concat"
+merge_transcoded_segments "$experiment_dir" "merge_with_ffmpeg_concat_demuxer"
 
 dump_frame_types_for_experiment "$experiment_dir"
