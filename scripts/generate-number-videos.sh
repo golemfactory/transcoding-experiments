20c23e8b720ae0095f392d7d91f17936d8815dc9
Kamil Śliwak <kamil.sliwak@codepoets.it>
2019-03-09 01:04:41 +0100

    generate-number-videos: Refactor to make it possible to generate different resolutions for different formats

--- scripts/generate-number-videos.sh
+++ scripts/generate-number-videos.sh
@@ -5,40 +5,55 @@ mkdir --parents number-videos/
 
 num_frames="$1"
 
+function generate_frame {
+    local frame_prefix="$1"
+    local frame_number="$2"
+    local width="$3"
+    local height="$4"
+
+    local dimensions=${width}x${height}
+    local font_size=$(( height / 2 ))
+
+    convert                     \
+        -gravity    Center      \
+        -background blue        \
+        -fill       white       \
+        -pointsize  $font_size  \
+        label:$frame_number     \
+        -extent     $dimensions \
+        -quality    100         \
+        "number-frames/$frame_prefix-$num_frames-$frame_number.png"
+}
+
+function generate_number_video {
+    local frame_prefix="$1"
+    local codec="$2"
+    local format="$3"
+
+    echo "Generating number-videos/numbers-$num_frames-$codec.$format"
+
+    ffmpeg                                                       \
+        -nostdin                                                 \
+        -v      error                                            \
+        -i      "number-frames/$frame_prefix-$num_frames-%d.png" \
+        -vcodec "$codec"                                         \
+        -strict -2                                               \
+        "number-videos/numbers-$num_frames-$codec.$format"
+}
+
 echo "Generating $num_frames number frames"
 
 # NOTE: The H.263 codec supports only a few specific resolutions. 352x288 is one of them.
 for i in $(seq 1 "$num_frames"); do
-    convert                 \
-        -gravity    Center  \
-        -background blue    \
-        -fill       white   \
-        -pointsize  200     \
-        label:$i            \
-        -extent     352x288 \
-        -quality    100     \
-        "number-frames/$num_frames-$i.png"
+    generate_frame default $i 352 288
 done
 
-function generate_number_video {
-    local codec="$1"
-    local format="$2"
+generate_number_video default flv1       flv
+generate_number_video h263    h263       3gp
+generate_number_video default h264       mp4
+generate_number_video default mpeg4      mpeg
+generate_number_video default theora     ogv
+generate_number_video default vp9        webm
+generate_number_video default vp9        mp4
+generate_number_video default wmv2       asf
 
-    echo "Generating number-videos/numbers-$num_frames-$codec.$format"
-
-    ffmpeg                                     \
-        -nostdin                               \
-        -v error                               \
-        -i "number-frames/$num_frames-%d.png"  \
-        -vcodec "$codec"                       \
-        "number-videos/numbers-$num_frames-$codec.$format"
-}
-
-generate_number_video vp9    mp4
-generate_number_video theora ogv
-generate_number_video flv1   flv
-generate_number_video mpeg4  mpeg
-generate_number_video h264   mp4
-generate_number_video vp9    webm
-generate_number_video wmv2   asf
-generate_number_video h263   3gp
