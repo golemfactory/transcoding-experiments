e861ad7d7e71402054ebc9eea8497b8a2374fad3
Kamil Śliwak <kamil.sliwak@codepoets.it>
2019-03-09 01:06:06 +0100

    generate-number-videos: Add more codec/container combinations

--- scripts/generate-number-videos.sh
+++ scripts/generate-number-videos.sh
@@ -46,14 +46,65 @@ echo "Generating $num_frames number frames"
 # NOTE: The H.263 codec supports only a few specific resolutions. 352x288 is one of them.
 for i in $(seq 1 "$num_frames"); do
     generate_frame default $i 128 128
+    generate_frame h261    $i 352 288   # Resolutions supported by h261:             176x144, 352x288
+    generate_frame h263    $i 352 288   # Resolutions supported by h263:     128x96, 176x144, 352x288,   704x576, 1408x1152
+    generate_frame dvvideo $i 720 576   # Resolutions supported by dvvideo: 720x480, 720x576, 960x720, 1280x1080, 1440x1080
 done
 
-generate_number_video default flv1       flv
-generate_number_video h263    h263       3gp
-generate_number_video default h264       mp4
-generate_number_video default mpeg4      mpeg
-generate_number_video default theora     ogv
-generate_number_video default vp9        webm
+# Tier 1: popular codecs
+generate_number_video default av1        mkv   # Alliance for Open Media AV1
+generate_number_video default cinepak    cpk   # Cinepak
+generate_number_video default dirac      drc   # Dirac
+generate_number_video default dirac      mkv
+generate_number_video default dirac      mp4
+generate_number_video default dirac      avi
+generate_number_video default flv1       flv   # FLV / Sorenson Spark / Sorenson H.263 (Flash Video)
+generate_number_video default flv1       mkv
+generate_number_video default flv1       avi
+generate_number_video h263    h263       3gp   # H.263 / H.263-1996, H.263+ / H.263-1998 / H.263 version 2
+generate_number_video h263    h263       h263
+generate_number_video default h264       mp4   # H.264 / AVC / MPEG-4 AVC / MPEG-4 part 10
+generate_number_video default h264       h264
+generate_number_video default h264       mkv
+generate_number_video default h264       avi
+generate_number_video default h264       flv
+generate_number_video default hevc       mp4   # H.265 / HEVC (High Efficiency Video Coding)
+generate_number_video default hevc       mkv
+generate_number_video default hevc       hevc
+generate_number_video default hevc       mpeg
+generate_number_video default mjpeg      mjpeg # Motion JPEG
+generate_number_video default mjpeg      avi
+generate_number_video default mjpeg      mov
+generate_number_video default mjpeg      mkv
+generate_number_video default mpeg1video mpeg  # MPEG-1 video
+generate_number_video default mpeg1video m1v
+generate_number_video default mpeg2video mpeg  # MPEG-2 video
+generate_number_video default mpeg4      mpeg  # MPEG-4 part 2
+generate_number_video default mpeg4      mp4
+generate_number_video default theora     ogv   # Theora
+generate_number_video default theora     mkv
+generate_number_video default vp8        webm  # On2 VP8
+generate_number_video default vp8        ivf
+generate_number_video default vp9        webm  # Google VP9
 generate_number_video default vp9        mp4
-generate_number_video default wmv2       asf
+generate_number_video default vp9        avi
+generate_number_video default wmv1       asf   # Windows Media Video 7
+generate_number_video default wmv2       asf   # Windows Media Video 8
 
+# Tier 2: some less popular codecs
+generate_number_video dvvideo dvvideo    dv    # DV (Digital Video)
+generate_number_video h261    h261       h261  # H.261
+generate_number_video h263    h263p      h263  # H.263+ / H.263-1998 / H.263 version 2
+generate_number_video default huffyuv    mkv   # HuffYUV
+generate_number_video default msmpeg4v2  mpeg  # MPEG-4 part 2 Microsoft variant version 2
+generate_number_video default msmpeg4v3  mpeg  # MPEG-4 part 2 Microsoft variant version 3
+generate_number_video default rawvideo   mkv   # raw video
+generate_number_video default rv10       rm    # RealVideo 1.0
+generate_number_video default rv20       rm    # RealVideo 2.0
+generate_number_video default svq1       mov   # Sorenson Vector Quantizer 1 / Sorenson Video 1 / SVQ1
+generate_number_video default v210       avi   # Uncompressed 4:2:2 10-bit
+generate_number_video default v308       avi   # Uncompressed packed 4:4:4
+generate_number_video default v408       avi   # Uncompressed packed QT 4:4:4:4
+generate_number_video default v410       avi   # Uncompressed 4:4:4 10-bit
+generate_number_video default y41p       avi   # Uncompressed YUV 4:1:1 12-bit
+generate_number_video default yuv4       avi   # Uncompressed packed 4:2:0
