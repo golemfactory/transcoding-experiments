5ce9ab8afa72c9f57d8fb3a012e569cb139ca9ee
Kamil Śliwak <kamil.sliwak@codepoets.it>
2019-03-11 11:32:11 +0100

    Add new splitting method for use in experiments: input_ss

--- scripts/functions/split-and-merge.sh
+++ scripts/functions/split-and-merge.sh
@@ -23,6 +23,76 @@ function ffmpeg_segment {
 }
 
 
+function ffmpeg_input_ss {
+    start_time="$1"
+    end_time="$2"
+    input_file="$3"
+    output_file="$4"
+
+    if [[ "$start_time" != "" ]]; then
+        ss_option="-ss $start_time"
+    else
+        ss_option=""
+    fi
+
+    if [[ "$end_time" != "" ]]; then
+        to_option="-to $end_time"
+    else
+        to_option=""
+    fi
+
+    echo "ffmpeg_input_ss: start=$start_time; end=$end_time; input=$input_file"
+
+    ffmpeg                \
+        -nostdin          \
+        -hide_banner      \
+        -v  error         \
+        $ss_option        \
+        $to_option        \
+        -i  "$input_file" \
+        -c  copy          \
+        "$output_file"
+}
+
+function ffmpeg_input_ss_loop {
+    local segment_duration="$1"
+    local video_duration="$2"
+    local input_file="$3"
+    local segment_name_prefix="$4"
+    local segment_format="$5"
+    local segment_list_file="$6"
+
+    printf "" > "$segment_list_file"
+
+    local segment_index=1
+    local start_time=""
+
+    if (( $(echo "${segment_duration}>${video_duration}" | bc --mathlib) )); then
+        local end_time="$video_duration"
+    elif (( $(echo "${segment_duration}==0" | bc --mathlib) )); then
+        local end_time="$video_duration"
+    else
+        local end_time="$segment_duration"
+    fi
+
+    while (( $(echo "${end_time}<${video_duration}" | bc --mathlib) )); do
+        local segment_file="$(printf "%s-%05d.%s" "$segment_name_prefix" "$segment_index" "$segment_format")"
+        printf "%s\n" "$(basename "$segment_file")" >> "$segment_list_file"
+
+        ffmpeg_input_ss "$start_time" "$end_time" "$input_file" "$segment_file"
+
+        segment_index=$(( $segment_index + 1 ))
+        start_time="$end_time"
+        end_time="$(echo "$end_time+$segment_duration" | bc --mathlib | sed 's/^\./0./')"
+    done
+
+    local segment_file="$(printf "%s-%05d.%s" "$segment_name_prefix" "$segment_index" "$segment_format")"
+    printf "%s\n" "$(basename "$segment_file")" >> "$segment_list_file"
+
+    ffmpeg_input_ss "$start_time" "" "$input_file" "$segment_file"
+}
+
+
 function ffmpeg_concat_demuxer {
     local ffconcat_segment_list_file="$1"
     local output_file="$2"
@@ -102,6 +172,29 @@ function split_with_ffmpeg_segment {
         "$segment_duration"                   \
         "$input_file"                         \
         "$output_dir/segment-%05d.$extension" \
+        "$output_dir/segments.txt"
+}
+
+
+function split_with_ffmpeg_input_ss {
+    local num_segments="$1"
+    local input_file="$2"
+    local output_dir="$3"
+
+    echo "Splitting $input_file into approximately $num_segments segments"
+
+    mkdir --parents $output_dir
+
+    local video_duration="$(video_duration_in_seconds "$input_file")"
+    local segment_duration=$(approximate_segment_length "$video_duration" "$num_segments")
+    local extension=$(get_extension "$input_file")
+
+    ffmpeg_input_ss_loop      \
+        "$segment_duration"   \
+        "$video_duration"     \
+        "$input_file"         \
+        "$output_dir/segment" \
+        "$extension"          \
         "$output_dir/segments.txt"
 }
 
